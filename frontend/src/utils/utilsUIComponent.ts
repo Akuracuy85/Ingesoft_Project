b0f8d26c01440337f1c59cd4531d6d66006a5189
AaronSH123 <163920863+AaronSH123@users.noreply.github.com>
2025-10-26 20:19:08 -0500

    Update name util

--- frontend/src/utils/utilsUIComponent.ts
+++ frontend/src/utils/utilsUIComponent.ts
@@ -1,3 +1,6 @@
+//usa internamente para manejar las clases de Tailwind CSS de forma más limpia y segura.
+//Lo genera automáticamente cuando haces npx shadcn@latest init
+
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
