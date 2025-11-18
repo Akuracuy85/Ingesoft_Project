565a31af0d7b91a377d6b887e4ae2babf3a1e82c
Krlos <cherediar123@gmail.com>
2025-11-17 22:48:27 -0500

    agregado recordar tarjeta

--- backend/src/routes/Perfil.routes.ts
+++ backend/src/routes/Perfil.routes.ts
@@ -5,12 +5,15 @@ import { sessionMiddleware } from "../middlewares/SessionMiddleware";
 const router = Router();
 
 // PUT /api/perfil
+router.get('/', sessionMiddleware.VerificarToken, perfilController.obtenerPerfil);
+
+router.get("/puntos", sessionMiddleware.VerificarToken, perfilController.obtenerPuntos);
+
 router.put("/", sessionMiddleware.VerificarToken, perfilController.actualizarPerfil);
 
-router.get('/', sessionMiddleware.VerificarToken, perfilController.obtenerPerfil);
+router.post("/tarjeta", sessionMiddleware.VerificarToken, perfilController.agregarTarjeta);
 
 router.delete("/tarjeta/:tarjetaId", sessionMiddleware.VerificarToken, perfilController.eliminarTarjeta);
 
-router.get("/puntos", sessionMiddleware.VerificarToken, perfilController.obtenerPuntos);
 
 export default router;
